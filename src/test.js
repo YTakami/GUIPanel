db90ff69b92dfdac06548ab828fb8b358712a1ff
sidestepism <fushimi.ryohe.i@gmail.com>
2014-10-05 22:05:22 -1000

    UIST2014が動いた

--- src/test.js
+++ src/test.js
@@ -1,32 +1,61 @@
 // KPR Script file
-exports.heart = function(){
-	var heart = [
-		[0,0,0,1,1,1,0,0,0,0,1,1,1,0,0,0],
-		[0,0,1,0,0,0,1,0,0,1,0,0,0,1,0,0],
-		[0,1,0,0,0,0,0,1,1,0,0,0,0,0,1,0],
-		[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-		[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-		[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-		[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-		[0,1,0,0,0,0,0,0,0,0,0,0,0,0,1,0],
-		[0,0,1,0,0,0,0,0,0,0,0,0,0,1,0,0],
-		[0,0,0,1,0,0,0,0,0,0,0,0,1,0,0,0],
-		[0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0],
-		[0,0,0,0,0,1,0,0,0,0,1,0,0,0,0,0],
-		[0,0,0,0,0,0,1,0,0,1,0,0,0,0,0,0],
-		[0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0]
-	];
-	return heart;
+var Matrix = require("node_modules/tinybitmap/index");
+
+exports.heart = function() {
+    var heart = [
+        [0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
+        [0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0],
+        [0, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+        [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+        [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+        [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0]
+    ];
+    return heart;
 }
-exports.uist = function(){
-	trace(1);
-	var Matrix = require("index");
-	trace(Matrix);
-	var m = new Matrix();
-	m.writeString("UIST");
-	m.breakLine();
-	m.putSpace();
-	m.writeString("2014");
-	m.printBitmap();
-	return m.getBitmap();
+
+exports.uist = function() {
+    var m = new Matrix();
+    m.writeString("UIST");
+    m.breakLine();
+    m.putSpace();
+    m.writeString("2014");
+    // m.printBitmap();
+    return m.getBitmap();
+}
+
+exports.mk10 = function() {
+    var m = new Matrix();
+    m.writeString("mk10");
+    m.breakLine();
+    m.putSpace();
+    m.writeString("1199");
+    return m.getBitmap();
+}
+
+exports.aloha = function() {
+    var m = new Matrix();
+    m.writeString(">ALO|>HA!");
+    m.breakLine();
+    m.printBitmap();
+    module.exports = m.getBitmap();
+}
+
+exports.kuku = function() {
+    var m = new Matrix();
+
+    function rand() {
+        return 3 + Math.floor(Math.random() * 7);
+    }
+    m.writeString(rand() + "*" + rand() + "?");
+    m.breakLine();
+    m.writeString(rand() + "*" + rand() + "?");
+    return m.getBitmap();
 }
